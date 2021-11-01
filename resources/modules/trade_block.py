fe01fd34abbc248c778f653af63a5ae26a0b8c50
elnathanau1 <elnathanau1@gmail.com>
2021-10-31 23:34:41 -0500

    Update trade block pt 2

--- resources/modules/trade_block.py
+++ resources/modules/trade_block.py
@@ -1,18 +1,21 @@
 import pandas as pd
 from dash import dash_table
-from resources.espn_fantasy_service import get_player_info, get_player_headshot
+from resources.espn_fantasy_service import get_player_info, get_player_headshot, get_team
 
 
 def generate_trade_block_chart(trade_block):
     # list to turn into dataframe later
     trade_block_table = []
     for trade in trade_block:
+        team = get_team(int(trade['team_id']))
         player_id = trade['player_id']
-        new_row = {
-            'Team': trade['team_id'],
-            'Player': '![]({0}){1}'.format(get_player_headshot(player_id), get_player_info(player_id)['fullName'])
-        }
-        trade_block_table.append(new_row)
+        # make sure only players on current rosters are on trade block
+        if str(player_id) in list(map(lambda player: str(player.playerId), team.roster)):
+            new_row = {
+                'Team': trade['team_name'],
+                'Player': '![]({0}){1}'.format(get_player_headshot(player_id), get_player_info(player_id)['fullName'])
+            }
+            trade_block_table.append(new_row)
 
     # turn into pandas dataframe
     trade_block_df = pd.DataFrame(trade_block_table)
