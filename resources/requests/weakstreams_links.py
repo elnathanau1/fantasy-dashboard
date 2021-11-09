1761f910c852301774563202233633de5ed2d1ec
elnathanau1 <elnathanau1@gmail.com>
2021-11-09 10:27:55 -0600

    Fix stream bugs

--- resources/requests/weakstreams_links.py
+++ resources/requests/weakstreams_links.py
@@ -1,15 +1,23 @@
 from datetime import date
-
+from dateutil.parser import parse
+import pytz
 import pandas as pd
 from pandas import DataFrame
 from bs4 import BeautifulSoup
 import requests
+from typing import Optional
+
+est = pytz.timezone('US/Eastern')
+fmt = '%B %d, %Y %-I:%M %p %Z'
 
 
-def weakstream_src_link(weakstream_link: str) -> str:
+def weakstream_src_link(weakstream_link: str) -> Optional[str]:
     r = requests.get(weakstream_link)
     soup = BeautifulSoup(r.text, 'html.parser')
-    iframe = BeautifulSoup(soup.find('textarea').text, 'html.parser')
+    textarea = soup.find('textarea')
+    if textarea is None:
+        return None
+    iframe = BeautifulSoup(textarea.text, 'html.parser')
     return iframe.find('iframe')['src']
 
 
@@ -28,11 +36,13 @@ def get_today_game_streams() -> list:
 
     r = requests.get(
         'https://site.api.espn.com/apis/fantasy/v2/games/fba/games',
-        params={'date': date.today().strftime('%Y%m%d')}
+        params={'dates': date.today().strftime('%Y%m%d')}
     )
     games_json = r.json()
     games = list(map(
         lambda game: {
+            'id': game['id'],
+            'date': parse(game['date'], fuzzy=True).astimezone(est).strftime(fmt),
             'detail': game['fullStatus']['type']['detail'],
             'percent_complete': game['percentComplete'],
             'teams': list(map(lambda team: team['name'], game['competitors'])),
@@ -46,10 +56,12 @@ def get_today_game_streams() -> list:
     for game in games:
         teams = list(map(lambda team: team.lower().replace(' ', '-'), game['teams']))
         result_df = df[df['loc'].str.contains(teams[0]) & df['loc'].str.contains(teams[1])]
-        stream_url = result_df.iloc[-1]['loc']
+
+        urls = list(map(lambda link: weakstream_src_link(link), result_df['loc'].to_list()))
+        urls = [x for x in urls if x is not None]
         game_stream_list.append({
-            'name': f"{game['teams'][0]} vs {game['teams'][1]}",
-            'stream_url': weakstream_src_link(stream_url)
+            'name': f"{game['teams'][0]} vs {game['teams'][1]} - {game['date']}",
+            'stream_url': urls[-1]
         })
 
     return game_stream_list
